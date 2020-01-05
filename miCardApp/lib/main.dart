cbc8d407e92e6c6fd82a9d717c63fa81e91653a4
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-01-05 06:21:32 +0530

    Card W replaces Container W & added Padding  class

--- miCardApp/lib/main.dart
+++ miCardApp/lib/main.dart
@@ -45,56 +45,62 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 //Use margin to add space from other widgets (space outside Container)
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0 ),
                 //Use padding to add space inside of the container and content
-                padding: EdgeInsets.all(10.0),
+                //padding: EdgeInsets.all(10.0),
                 //Adding a container bought the entire content in the center of the screen as Container widget
                 // with no child occupies the maximum space.
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Text(
-                      "+91 810 990 9272",
-                      style: TextStyle(
-                        color: Colors.teal,
-                        fontFamily: "Source Sans Pro",
-                        fontSize: 20.0,
-                      )
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "+91 810 990 9272",
+                        style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20.0,
+                        )
+                      ),
+                    ],
+                  ),
                 ), 
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Text(
-                      "krishnatandon.1208@gmail.com",
-                      style: TextStyle(
-                        fontFamily: "Source Sans Pro",
-                        fontSize: 20.0,
+                //padding: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
                         color: Colors.teal,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "krishnatandon.1208@gmail.com",
+                        style: TextStyle(
+                          fontFamily: "Source Sans Pro",
+                          fontSize: 20.0,
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ),
             ],
