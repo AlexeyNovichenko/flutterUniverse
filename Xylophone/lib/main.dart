173d8954034ea4e3ec59e91be6c7f37a6289c39e
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-05-05 06:06:11 +0530

    xylophone single instance

--- Xylophone/lib/main.dart
+++ Xylophone/lib/main.dart
@@ -1,28 +1,30 @@
 import "package:flutter/material.dart";
-import "package:english_words/english_words.dart";
-
+import "package:audioplayers/audio_cache.dart";
 
 void main() {
-	runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  	@override
-  	Widget build(BuildContext context) {
-    	return (MaterialApp(
-      		theme: ThemeData.dark(),
-      		debugShowCheckedModeBanner: false,
-      		home: Scaffold(
-			appBar: AppBar(
-          		title: Text("Xylophone App"),
-          		backgroundColor: Colors.lightBlueAccent,
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Xylophone App"),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        body: SafeArea(
+          child: Container(
+              child: Center(child: FlatButton(onPressed: (){
+				  final player = AudioCache();
+				  player.play("note1.wav");
+				}, 
+				child: Text("Click me")))),
         	),
-			body: SafeArea(
-          		child: Container(
-					  child: Center(child: Text(nouns.last))
-				  ),
-        	),
-		),
+      	),
     ));
   }
 }
