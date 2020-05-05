8f581b629bc6ee1b4424801b4a25bbc34a555cde
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-05-05 18:48:05 +0530

    Xylophone created

--- Xylophone/lib/main.dart
+++ Xylophone/lib/main.dart
@@ -6,6 +6,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final sound = AudioCache();
+    sound.play("note$soundNumber.wav");
+  }
+
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
@@ -17,14 +22,53 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.lightBlueAccent,
         ),
         body: SafeArea(
-          child: Container(
-              child: Center(child: FlatButton(onPressed: (){
-				  final player = AudioCache();
-				  player.play("note1.wav");
-				}, 
-				child: Text("Click me")))),
-        	),
-      	),
+            child: Column(
+          children: [
+            FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text("1")),
+            FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text("2")),
+            FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text("3")),
+            FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text("4")),
+            FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text("5")),
+            FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text("6")),
+            FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text("7")),
+          ],
+        )),
+      ),
     ));
   }
 }
